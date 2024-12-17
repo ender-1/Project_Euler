3a778843ee3ec7667e666e837066e1c7f85a6011
Aashutosh <aashutoshswami777@gmail.com>
2024-12-17 12:15:36 +0530

    Update PE_Problem_1.c

--- PE_Problem_1.c
+++ PE_Problem_1.c
@@ -1,34 +1,34 @@
 /** *****************************************************************
-						Multiples of 3 or 5
+			Multiples of 3 or 5
   
-  Q1. A palindromic number reads the same both ways. The largest 
-      palindrome made from the product of two -digit numbers is 
-	  9009 = 91 x 99.
+  Q1. If we list all the natural numbers below 10 that are multiples 
+      of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 
+	  23.
+
+	  Find the sum of all the multiples of 3 or 5 below 1000.
 	  
-	  Find the largest palindrome made from the product of two 
-	  -digit numbers.
-	  
-  A1. 906609   
+  A1. 233168   
 ***************************************************************** **/
 
 #include <stdio.h>
+#include <stdint.h>
 
 /** ***********************************************8
      This function calculates the sum of numbers 
 	 divisible by n up to target. It uses the 
 	 arithmetic series formula.
 ************************************************ **/
-int sum_divisible_by(int n, int target) {
-    int p = target / n; 
+uint32_t sum_divisible_by(uint32_t n, uint32_t target) {
+    uint32_t p = target / n; 
     return n * (p * (p + 1)) / 2; 
 }
 
 int main() {
-    int sum = sum_divisible_by(3, 999) + 
+    uint32_t sum = sum_divisible_by(3, 999) + 
               sum_divisible_by(5, 999) - 
               sum_divisible_by(15, 999);
 
-    printf("The sum of all multiples of 3 or 5 below 1000 is %d.\n", sum);
+    printf("The sum of all multiples of 3 or 5 below 1000 is %u.\n", sum);
     return 0;
 }
 
