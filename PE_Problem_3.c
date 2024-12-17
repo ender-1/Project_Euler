ba4c9573b7573e79d41f8347fc6ad57352a26dc5
Aashutosh <aashutoshswami777@gmail.com>
2024-12-17 12:18:30 +0530

    Update PE_Problem_3.c

--- PE_Problem_3.c
+++ PE_Problem_3.c
@@ -1,5 +1,5 @@
 /** *****************************************************************
-						Largest Prime Factor
+			Largest Prime Factor
 
   Q3. The prime factors of 13195 are 5, 7, 13 and 29.
       What is the largest prime factor of the number 600851475143 ?
@@ -8,14 +8,15 @@
 ***************************************************************** **/
 
 #include <stdio.h>
+#include <stdint.h>
 
 int main() {
-    long long n = 600851475143LL; // Use long long and LL suffix
-	long long m;
+    uint64_t n = 600851475143LL; // Use uint64_t and LL suffix
+	uint64_t m;
 	m = n;
-	long long pf = 2; //Assuming that the largest prime factor is 2
-	int i;
-	long long f;
+	uint64_t pf = 2; //Assuming that the largest prime factor is 2
+	uint32_t i;
+	uint64_t f;
 	
 // Removes all factors of 2
 	
@@ -32,32 +33,32 @@ int main() {
 	
 /** ****************************************************************
     The loop iterates through potential odd prime factors of n.
-	The loop variable starts with 5 as we are using the 
-	6k ± 1 Optimization which is based on the mathematical property 
-	that all prime numbers greater than 3 can be expressed in the 
-	form 6k ± 1, where k is any integer. By starting with f = 5 and 
-	incrementing it by 2 and 4 alternately, the loop efficiently 
-	checks all numbers of this form, effectively covering all 
-	potential prime factors greater than 3.
-	
-	n /= f: This divides the number n by prime factor pf 
-	repeatedly to remove all instances of the prime factor pf 
-	from the number n. This helps f += i: In each iteration,
-	f is incremented by i.
-	
-	i = 6 - i: This alternates the value of i between 2 
-	and 4 (2 -> 4 -> 2 -> 4 ...), effectively generating the 
-	sequence 5, 7, 11, 13, 17, 19, and so on.
-	
-	If n is divisible by f, it means f is a prime factor.
-	
-	pf = f: The pf variable which stores the largest prime 
-	factor found so far is updated to f.
-	
-	n /= f: The code divides n by f repeatedly to remove all 
-	occurrences of this prime factor from n. This is crucial for 
-	efficiency because it reduces the size of n and helps in 
-	finding the next largest prime factor more quickly.
+    The loop variable starts with 5 as we are using the 
+    6k ± 1 Optimization which is based on the mathematical property 
+    that all prime numbers greater than 3 can be expressed in the 
+    form 6k ± 1, where k is any integer. By starting with f = 5 and 
+    incrementing it by 2 and 4 alternately, the loop efficiently 
+    checks all numbers of this form, effectively covering all 
+    potential prime factors greater than 3.
+ 
+   n /= f: This divides the number n by prime factor pf 
+   repeatedly to remove all instances of the prime factor pf 
+   from the number n. This helps f += i: In each iteration,
+   f is incremented by i.
+
+  i = 6 - i: This alternates the value of i between 2 
+  and 4 (2 -> 4 -> 2 -> 4 ...), effectively generating the 
+  sequence 5, 7, 11, 13, 17, 19, and so on.
+
+  If n is divisible by f, it means f is a prime factor.
+
+  pf = f: The pf variable which stores the largest prime 
+  factor found so far is updated to f.
+
+  n /= f: The code divides n by f repeatedly to remove all 
+  occurrences of this prime factor from n. This is crucial for 
+  efficiency because it reduces the size of n and helps in 
+  finding the next largest prime factor more quickly.
 ***************************************************************  **/
     for (f = 5, i = 2; f * f <= n; f += i, i = 6 - i) {
         while (n % f == 0) {
@@ -68,14 +69,14 @@ int main() {
 
 /** ****************************************************************
     If above test conditions fail then this checks if n > 1
-	and if n > 1 then it implies that n is itself a prime number
+    and if n > 1 then it implies that n is itself a prime number
 ***************************************************************  **/
 
     if (n > 1) { 
         pf = n;
     }
 
-    printf("The largest prime factor of %lld is %lld.\n", m, pf); 
+    printf("The largest prime factor of %lld is %u.\n", m, pf); 
 
     return 0;
 }
