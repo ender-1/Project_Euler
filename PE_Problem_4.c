afad1e9000c68139a02dfce7719e67a74b01dff7
Aashutosh <aashutoshswami777@gmail.com>
2024-12-17 12:19:50 +0530

    Update PE_Problem_4.c

--- PE_Problem_4.c
+++ PE_Problem_4.c
@@ -1,23 +1,25 @@
 /** *****************************************************************
-						Largest Palindrome Product
+			Largest Palindrome Product
 
   Q4. A palindromic number reads the same both ways. The largest 
       palindrome made from the product of two -digit numbers is 
-	  9009 = 91 x 99.
 	  
-	  Find the largest palindrome made from the product of 
-	  two-digit numbers.
+    			9009 = 91 x 99.
+	  
+      Find the largest palindrome made from the product of 
+      two-digit numbers.
 	  
   A4. 906609   
 ***************************************************************** **/
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 // Function to check if a number is a palindrome
 bool is_palindrome(int num) {
-  int original = num;
-  int reversed = 0;
+  uint32_t original = num;
+  uint32_t reversed = 0;
 
   while (num > 0) {
     reversed = reversed * 10 + num % 10;
@@ -28,8 +30,8 @@ bool is_palindrome(int num) {
 }
 
 int main() {
-  int largest_palindrome = 0;
-  int i,j;
+  uint32_t largest_palindrome = 0;
+  uint32_t i,j;
 
 for (i = 999; i >= 100; i--) {
 /** *****************************************************************
@@ -39,7 +41,7 @@ a smaller j will also be smaller. Start j from 999 so can reach
 largest palindrome faster
 ***************************************************************** **/
         for (j = 999; j >= i; j--) { 
-            int product = i * j;
+            uint32_t product = i * j;
             if (is_palindrome(product)) {
                 if (product > largest_palindrome) {
                     largest_palindrome = product;
