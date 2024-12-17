6cd02cc6b5d8afad7fd0c507913e003745ce79dd
Aashutosh <aashutoshswami777@gmail.com>
2024-12-17 15:17:17 +0530

    Update PE_Problem_3.c

--- PE_Problem_3.c
+++ PE_Problem_3.c
@@ -10,6 +10,16 @@
 #include <stdio.h>
 #include <stdint.h>
 
+uint32_t is_prime(uint32_t n){
+	uint32_t i;
+	for (i = 5; i*i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 int main() {
     uint64_t n = 600851475143LL; // Use uint64_t and LL suffix
 	uint64_t m;
@@ -40,32 +50,35 @@ int main() {
     incrementing it by 2 and 4 alternately, the loop efficiently 
     checks all numbers of this form, effectively covering all 
     potential prime factors greater than 3.
- 
-   n /= f: This divides the number n by prime factor pf 
-   repeatedly to remove all instances of the prime factor pf 
-   from the number n. This helps f += i: In each iteration,
-   f is incremented by i.
-
-  i = 6 - i: This alternates the value of i between 2 
-  and 4 (2 -> 4 -> 2 -> 4 ...), effectively generating the 
-  sequence 5, 7, 11, 13, 17, 19, and so on.
-
-  If n is divisible by f, it means f is a prime factor.
-
-  pf = f: The pf variable which stores the largest prime 
-  factor found so far is updated to f.
-
-  n /= f: The code divides n by f repeatedly to remove all 
-  occurrences of this prime factor from n. This is crucial for 
-  efficiency because it reduces the size of n and helps in 
-  finding the next largest prime factor more quickly.
+	
+    n /= f: This divides the number n by prime factor pf 
+    repeatedly to remove all instances of the prime factor pf 
+    from the number n. This helps f += i: In each iteration,
+    f is incremented by i.
+	
+    i = 6 - i: This alternates the value of i between 2 
+    and 4 (2 -> 4 -> 2 -> 4 ...), effectively generating the 
+    sequence 5, 7, 11, 13, 17, 19, and so on.
+	
+    If n is divisible by f, it means f is a prime factor.
+	
+    pf = f: The pf variable which stores the largest prime 
+    factor found so far is updated to f.
+	
+    n /= f: The code divides n by f repeatedly to remove all 
+    occurrences of this prime factor from n. This is crucial for 
+    efficiency because it reduces the size of n and helps in 
+    finding the next largest prime factor more quickly.
 ***************************************************************  **/
-    for (f = 5, i = 2; f * f <= n; f += i, i = 6 - i) {
-        while (n % f == 0) {
-            pf = f;
-            n /= pf;
-        }
-    }
+    for (f = 5, i = 2; f * f <= n; f += i, i = 6 - i) {        
+	if(is_prime(f)){
+		pf = f;
+		while (n % f == 0) {
+			n /= f;
+		}
+	}
+   }
+ 
 
 /** ****************************************************************
     If above test conditions fail then this checks if n > 1
