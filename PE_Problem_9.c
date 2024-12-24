49d248cf66ea7cad0b7ab824b76c8ebb2fddba1d
Aashutosh <aashutoshswami777@gmail.com>
2024-12-24 18:11:32 +0530

    Update PE_Problem_9.c

--- PE_Problem_9.c
+++ PE_Problem_9.c
@@ -1,22 +1,22 @@
 /** *****************************************************************
-						Special Pythagorean Triplet
+			Special Pythagorean Triplet
 
   Q9. A Pythagorean triplet is a set of three natural numbers, 
   
-							a < b < c,
+			a < b < c,
 	
-	  for which,
+      for which,
 	  
-							(a^2) + (b^2) = (c^2).
+     			(a^2) + (b^2) = (c^2).
 
       For example, (3^2)+(4^2)= 9 + 16 = 25 = (5^2).
 
       There exists exactly one Pythagorean triplet for 
-	  which,
+      which,
 	  
-							a + b + c = 1000.
+			a + b + c = 1000.
 
-	  Find the product abc
+      Find the product abc
 	  
   A9. 31875000  
 ***************************************************************** **/
