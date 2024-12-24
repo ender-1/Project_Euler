dcb24452fc57d6040be955516a15b4277b303fbc
Aashutosh <aashutoshswami777@gmail.com>
2024-12-24 18:10:23 +0530

    Update PE_Problem_8.c

--- PE_Problem_8.c
+++ PE_Problem_8.c
@@ -1,32 +1,32 @@
 /** *****************************************************************
-						Largest Product in a Series
+			Largest Product in a Series
 
   Q8. The four adjacent digits in the 1000-digit number that have the 
       greatest product are 9×9×8×9=5832.
 
-				73167176531330624919225119674426574742355349194934
-				96983520312774506326239578318016984801869478851843
-				85861560789112949495459501737958331952853208805511
-				12540698747158523863050715693290963295227443043557
-				66896648950445244523161731856403098711121722383113
-				62229893423380308135336276614282806444486645238749
-				30358907296290491560440772390713810515859307960866
-				70172427121883998797908792274921901699720888093776
-				65727333001053367881220235421809751254540594752243
-				52584907711670556013604839586446706324415722155397
-				53697817977846174064955149290862569321978468622482
-				83972241375657056057490261407972968652414535100474
-				82166370484403199890008895243450658541227588666881
-				16427171479924442928230863465674813919123162824586
-				17866458359124566529476545682848912883142607690042
-				24219022671055626321111109370544217506941658960408
-				07198403850962455444362981230987879927244284909188
-				84580156166097919133875499200524063689912560717606
-				05886116467109405077541002256983155200055935729725
-				71636269561882670428252483600823257530420752963450
+	7316717653133062491922511967442657474235534919493
+        96983520312774506326239578318016984801869478851843
+	85861560789112949495459501737958331952853208805511
+ 	12540698747158523863050715693290963295227443043557
+	66896648950445244523161731856403098711121722383113
+	62229893423380308135336276614282806444486645238749
+	30358907296290491560440772390713810515859307960866
+	70172427121883998797908792274921901699720888093776
+	65727333001053367881220235421809751254540594752243
+	52584907711670556013604839586446706324415722155397
+	53697817977846174064955149290862569321978468622482
+	83972241375657056057490261407972968652414535100474
+	82166370484403199890008895243450658541227588666881
+	16427171479924442928230863465674813919123162824586
+	17866458359124566529476545682848912883142607690042
+	24219022671055626321111109370544217506941658960408
+	07198403850962455444362981230987879927244284909188
+	84580156166097919133875499200524063689912560717606
+	05886116467109405077541002256983155200055935729725
+	71636269561882670428252483600823257530420752963450
 
       Find the thirteen adjacent digits in the 1000-digit number that 
-	  have the greatest product. 
+      have the greatest product. 
 	  
   A8. 23514624000  
 ***************************************************************** **/
@@ -44,20 +44,20 @@
     Key Components:
 
     Window: A contiguous subarray or substring of the input. 
-	        It's typically defined by two pointers:
+            It's typically defined by two pointers:
 
             Left Pointer (Start): Marks the beginning of the window.
 
             Right Pointer (End): Marks the end of the window.
 			
     Window Size: The number of elements within the current window. 
-	             This size can be fixed or variable.
+                 This size can be fixed or variable.
 
     Sliding: The process of moving the window across the input. 
-	         This is done by either:
+             This is done by either:
 
 	Expanding the window: Incrementing the right pointer to include 
-						  more elements.
+		              more elements.
 						  
 	Shrinking the window: Incrementing the left pointer to exclude 
 	                      elements from the beginning.
@@ -78,17 +78,17 @@
 
 		Check Condition: Evaluate if the current window satisfies the 
 		                 desired condition (e.g., sum within a certain range, 
-						 contains all required characters).
+				 contains all required characters).
 						 
 		Shrink (if needed): If the condition is not met or if you need to 
-							optimize the window size, start shrinking the 
-							window by incrementing the left pointer and 
-							updating the variables accordingly. Continue 
-							shrinking until the condition is satisfied or a 
-							minimum window size is reached.
+			            optimize the window size, start shrinking the 
+				    window by incrementing the left pointer and 
+				    updating the variables accordingly. Continue 
+				    shrinking until the condition is satisfied or a 
+				    minimum window size is reached.
 							
 		Update Result: If the current window is a valid solution, update the 
-					   overall result (e.g., minimum/maximum length, best sum).
+			       overall result (e.g., minimum/maximum length, best sum).
 					   
 		Move the Window: Move the right pointer forward to continue the process.
 
